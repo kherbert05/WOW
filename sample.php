68d4b8fcc7a0588a2007a585dca9570665b2576b
kherbert05 <kherbert05@hotmail.com>
2014-05-08 03:57:42 -0700

    Update sample.php

--- sample.php
+++ sample.php
@@ -1,6 +1,8 @@
 <?php
+$access_token = $_GET['access_token'];
 echo "<html><body>";
 echo "<b>THIS IS A SAMPLE FANTASY PLATFORM APP</b>";
 echo "<br><br>";
+echo "token=<b>$access_token</b>";
 echo "</body></html>";
 ?>
